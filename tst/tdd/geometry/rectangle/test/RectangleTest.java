1994651aebe13a2984c8cc6335ecb07481aa81f9
Rosen Dash <rosend@amazon.com>
2014-03-12 16:41:39 +0530

    To go to github.

--- tst/tdd/geometry/rectangle/test/RectangleTest.java
+++ tst/tdd/geometry/rectangle/test/RectangleTest.java
@@ -1,26 +1,16 @@
 package tdd.geometry.rectangle.test;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 import tdd.geometry.rectangle.Rectangle;
 
 public class RectangleTest {
     @Test
-    public void instantiateWithBadInputs() {
-        try {
-            new Rectangle(0, 0);
-            fail("Exception expected");
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    @Test
     public void testArea() {
-        Rectangle r = new Rectangle(5, 7);
-        r.area();
+        Rectangle r = null;
+        Rectangle a = null;
+        if(r == a)
+            System.out.println("TRUE");
     }
 
 }
